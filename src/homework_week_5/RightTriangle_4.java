26a99d3718596c1bd7f1a9f5b0b6a68c1cda2ba4
Cucumber0078 <ashmore0757@gmail.com>
2022-10-13 21:42:04 +0100

    ExtrawritingHW

---
+++ src/homework_week_5/RightTriangle_4.java
@@ -0,0 +1,15 @@
+package homework_week_5;
+
+public class RightTriangle_4 {
+    public class RightTriangle4 {
+        public static void main(String[] args) {
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+            System.out.println("*****");
+            System.out.println("******");
+        }
+    }
+
+}
